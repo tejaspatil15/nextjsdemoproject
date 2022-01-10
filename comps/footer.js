241744f6d67aa3d7166268a2d0d52add0fd93df7
Tejas Patil <tejas_patil2@persistent.com>
2022-01-11 04:18:36 +0530

    First Commit Next

---
+++ comps/footer.js
@@ -0,0 +1,21 @@
+
+import styles from '../styles/Home.module.css'
+
+const Footer = () => {
+
+    return (
+        <>
+
+            <footer className={styles.footer}>
+                <div className={styles.text}>
+                    @copyright 2022 next.js
+                </div>
+            </footer>
+
+        </>
+    )
+}
+
+export default Footer;
+
+
---
+++ comps/navbar1.js
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+const Navbar1 = () => {
+
+    // }
+    // export default function Navbar1() {
+    return (
+        <>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <Link href="/"><a class="navbar-brand" href="#">Navbar</a></Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <Link href="/"><a class="nav-link active" aria-current="page">Home</a></Link>
+                            </li>
+
+                            <li class="nav-item">
+                                <Link href="/about"><a class="nav-link">About us</a></Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link href="/contact"><a class="nav-link">Contact us</a></Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link href="/components/new"><a class="nav-link">New</a></Link>
+                            </li>
+                        </ul>
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar1;
